884a593f49afd9189507a51e553f97e8e12482c7
berylatieno30 <berylatieno30@gmail.com>
2025-02-06 11:25:42 +0000

    add number response struct

---
+++ models/response.go
@@ -0,0 +1,10 @@
+package models
+
+type NumberResponse struct {
+	Number     int      `json:"number"`
+	IsPrime    bool     `json:"is_prime"`
+	IsPerfect  bool     `json:"is_perfect"`
+	Properties []string `json:"properties"`
+	DigitSum   int      `json:"digit_sum"`
+	FunFact    string   `json:"fun_fact"`
+}
