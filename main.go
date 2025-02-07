8dafc003efdeee89648bdc6a7c56fd77439fcee1
berylatieno30 <berylatieno30@gmail.com>
2025-02-07 14:33:20 +0000

    update endpoint

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 
 func main() {
 	// Define the endpoint handler
-	http.HandleFunc("/api/classify", handlers.ClassifyNumberHandler)
+	http.HandleFunc("/api/classify-number", handlers.ClassifyNumberHandler)
 
 	fmt.Println("Server running on 0.0.0.0:8080")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
